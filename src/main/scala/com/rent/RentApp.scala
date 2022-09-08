29bf8e60161449140644ad08ee8db62f30ebc19e
ElderHex <karasevrobert@gmail.com>
2022-09-08 17:16:22 +0400

    - I think it isn't alive now :( BUT something work, But I don't know what:)

--- src/main/scala/com/rent/RentApp.scala
+++ src/main/scala/com/rent/RentApp.scala
@@ -3,7 +3,6 @@ package com.rent
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior}
 import akka.cluster.typed.Cluster
-import com.rent.actor.Worker
 import com.rent.service.ChatService
 import com.typesafe.config.ConfigFactory
 import javafx.application.Application
@@ -20,14 +19,15 @@ object RentApplication {
         def apply(): Behavior[Nothing] = Behaviors.setup[Nothing] { ctx =>
             val cluster = Cluster(ctx.system)
 
-            if (cluster.selfMember.hasRole("backend")) {
-                val workersPerNode =
-                    ctx.system.settings.config.getInt("transformation.workers-per-node")
-                (1 to workersPerNode).foreach { n =>
-                    ctx.spawn(Worker(), s"Worker$n")
-                    println("SPAWN WORKER")
-                }
-            }
+
+//            if (cluster.selfMember.hasRole("backend")) {
+//                val workersPerNode =
+//                    ctx.system.settings.config.getInt("transformation.workers-per-node")
+//                (1 to workersPerNode).foreach { n =>
+//                    ctx.spawn(Worker(), s"Worker$n")
+//                    println("SPAWN WORKER")
+//                }
+//            }
             if (cluster.selfMember.hasRole("frontend")) {
                 ctx.spawn(ClientView(), "Frontend")
                 println("SPAWN FRONTEND")
@@ -44,14 +44,14 @@ object RentApplication {
           akka.remote.artery.canonical.port=$port
           akka.cluster.roles = [$role]
           """)
-            .withFallback(ConfigFactory.load("transformation"))
+            .withFallback(ConfigFactory.load())
 
         ActorSystem[Nothing](RootBehavior(), "ClusterSystem", config)
 
     }
      def main(args: Array[String]): Unit = {
-        startup("backend", 25251)
-        startup("backend", 25252)
+        //startup("frontend", 25251)
+        startup("frontend", 25252)
         Application.launch(classOf[RentApp])
     }
 
--- src/main/scala/com/rent/actor/ClientView.scala
+++ src/main/scala/com/rent/actor/ClientView.scala
@@ -1,45 +1,55 @@
 package viewChatController
 
-import akka.actor.typed.receptionist.Receptionist
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
-import com.rent.actor.Worker
-import com.rent.controller.ChatController
-import javafx.application.Platform
-import com.rent.model.Message
-import com.rent.service.ChatService
+import akka.actor.typed.{ActorRef, Behavior}
+import com.rent.utils.CborSerializable
 
 import scala.concurrent.duration.DurationInt
 
 object ClientView {
+
+    val ClientServiceKey = ServiceKey[ClientView.Event]("ClientView")
+
     sealed trait Event
 
     private case object Tick extends Event
 
-    private final case class WorkersUpdated(newWorkers: Set[ActorRef[Worker.TransformText]]) extends Event
+    private final case class WorkersUpdated(newWorkers: Set[ActorRef[ClientView.Event]]) extends Event
 
     private final case class TransformCompleted(originalText: String, transformedText: String) extends Event
 
     private final case class JobFailed(why: String, text: String) extends Event
 
+    //final case class TransformText(text: String, replyTo: ActorRef[TextTransformed]) extends Event with CborSerializable
+
+    //final case class TextTransformed(text: String) extends CborSerializable
+
     def apply(): Behavior[Event] = Behaviors.setup { ctx =>
-        Behaviors.withTimers { timers =>
+
+        ctx.log.info("Registering myself with receptionist")
+
+        //Behaviors.withTimers { timers =>
             // subscribe to available workers
             val subscriptionAdapter = ctx.messageAdapter[Receptionist.Listing] {
-                case Worker.WorkerServiceKey.Listing(workers) =>
+                case ClientView.ClientServiceKey.Listing(workers) =>
                     WorkersUpdated(workers)
             }
-            ctx.system.receptionist ! Receptionist.Subscribe(Worker.WorkerServiceKey, subscriptionAdapter)
+        ctx.system.receptionist ! Receptionist.Register(ClientServiceKey, ctx.self)
+        ctx.system.receptionist ! Receptionist.Subscribe(ClientView.ClientServiceKey, subscriptionAdapter)
 
-            timers.startTimerWithFixedDelay(Tick, Tick, 2.seconds)
+        //timers.startTimerWithFixedDelay(Tick, Tick, 2.seconds)
 
-            running(ctx, IndexedSeq.empty, jobCounter = 0)
-        }
+        //}
+        running(ctx, IndexedSeq.empty, jobCounter = 0)
     }
 
-    private def running(ctx: ActorContext[Event], workers: IndexedSeq[ActorRef[Worker.TransformText]], jobCounter: Int): Behavior[Event] =
-        Behaviors.receiveMessage {
-            case WorkersUpdated(newWorkers) =>
+    private def running(ctx: ActorContext[Event], workers: IndexedSeq[ActorRef[ClientView.Event]], jobCounter: Int): Behavior[Event] =
+    Behaviors.receiveMessage {
+        case WorkersUpdated(newWorkers) =>
+            println("Это ЗИС: " + ctx.self)
+            println("List: " + newWorkers)
+                println(newWorkers.size)
                 ctx.log.info("List of services registered with the receptionist changed: {}", newWorkers)
                 running(ctx, newWorkers.toIndexedSeq, jobCounter)
         }
